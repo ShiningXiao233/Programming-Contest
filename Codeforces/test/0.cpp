406ea8911f13e8e99e0ea59ec2335a35c4adbe02
405 <405@xzl.com>
2023-07-08 17:55:00 +0800

    2023-07-08 17:55:00

---
+++ Codeforces/test/0.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 1e5 + 100;
+const int MOD = 998244353;
+
+void sol() {
+    
+}
+
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    // cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        sol();
+    }
+}
