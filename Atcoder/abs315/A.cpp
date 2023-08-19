65ad4e4e8d599bbb633a010440e725ad2e02afa4
405 <405@xzl.com>
2023-08-19 19:07:11 +0800

    2023-08-19 19:07:11

---
+++ Atcoder/abs315/A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+const int N = 1e5 + 100;
+const int MOD = 998244353;
+
+void sol() {
+    
+}
+
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    // cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        sol();
+    }
+    exit(0);
+}
