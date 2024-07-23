ae3b4e66f9fa85184e28a470c8b1d197b185eaa5
aruro <aruro.xzl@foxmail.com>
2024-07-23 11:22:03 +0800

    2024-07-23-11:22:03

--- push.py
+++ push.py
@@ -1,20 +1,23 @@
 import subprocess
+import time
+import sys
+
 
 def exec_cmd(info : str, cmd : str):
     cmd_list = list(cmd.split(' '))
-    print(cmd_list)
+    # print(cmd_list)
     rp = subprocess.run(cmd_list, capture_output=True, encoding='utf-8')
     print(info, rp.returncode, rp.stdout)
     if rp.returncode != 0:
         print(rp.stderr)
         exit(0)
 
-exec_cmd("pull", "git pull")
-exec_cmd("add", "git add .")
-
-import time
-info = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
-print(info)
-
-exec_cmd("commit", f"git commit -m {info}")
-exec_cmd("push", "git push")
+if __name__ == '__main__':
+    print(sys.argv)
+    exec_cmd("pull", "git pull")
+    exec_cmd("add", "git add .")
+    
+    info = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
+    # print(info)
+    exec_cmd("commit", f"git commit -m {info}")
+    exec_cmd("push", "git push")
