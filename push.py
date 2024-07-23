78d00a464e9ffe09adb0d40895f7a4ca9bbf9143
aruro <aruro.xzl@foxmail.com>
2024-07-23 11:24:28 +0800

    python-push

--- push.py
+++ push.py
@@ -7,7 +7,7 @@ def exec_cmd(info : str, cmd : str):
     cmd_list = list(cmd.split(' '))
     # print(cmd_list)
     rp = subprocess.run(cmd_list, capture_output=True, encoding='utf-8')
-    print(info, rp.returncode, rp.stdout)
+    print(info, rp.returncode == 0, rp.stdout)
     if rp.returncode != 0:
         print(rp.stderr)
         exit(0)
